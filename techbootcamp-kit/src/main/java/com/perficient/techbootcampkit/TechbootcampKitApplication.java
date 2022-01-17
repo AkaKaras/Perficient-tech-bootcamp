3e5cb846d1984688a385f9f37f200cc3a955da63
kit.lam <kit.lam@perficient.com>
2022-01-17 17:28:09 -0500

    add file

---
+++ techbootcamp-kit/src/main/java/com/perficient/techbootcampkit/TechbootcampKitApplication.java
@@ -0,0 +1,13 @@
+package com.perficient.techbootcampkit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechbootcampKitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechbootcampKitApplication.class, args);
+	}
+
+}
---
+++ techbootcamp-kit/src/test/java/com/perficient/techbootcampkit/TechbootcampKitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perficient.techbootcampkit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechbootcampKitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
